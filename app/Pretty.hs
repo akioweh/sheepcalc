46e76da0778714bbe9aa3fddad949a5dbb63cb6e
akioweh <0@akioweh.com>
2026-05-07 22:17:52 +0100

    pretty print church numerals as digits

--- app/Pretty.hs
+++ app/Pretty.hs
@@ -7,15 +7,44 @@ import TextBuilder
 
 import DeBruijn
 
--- | pretty printer
+-- | precedence levels — controls when subterms get parenthesized
+topP, lamP, appP, argP :: Int
+topP = 0 -- top-level / inside a lambda body
+lamP = 1 -- abstraction `λ. m`
+appP = 2 -- application `f x`
+argP = 3 -- right-hand side of an application
+
+-- | pretty printer with Church numeral detection
 pretty :: DExpr -> Text
-pretty = toText . go 0
- where
-  go :: Int -> DExpr -> TextBuilder
-  go _ (BoundVar v) = string . show $ v
-  go _ (FreeVar v) = string v
-  go p (DAbs m) = _paren p 1 $ "λ." <> go 0 m
-  go p (DApp x y) = _paren p 2 $ go 2 x <> " " <> go 3 y
-  _paren p q t
-    | p > q = "(" <> t <> ")"
-    | otherwise = t
+pretty = toText . pstr . walk topP
+
+{- | result of one bottom-up walk
+`num` is `Just n` iff this expression is a Church body chain `1 (1 ... (1 0))` of length n
+`pstr`  is the pretty-printed form
+-}
+data Walk = Walk {num :: Maybe Int, pstr :: TextBuilder}
+
+-- | Church numeral extraction thing (linear time)
+walk :: Int -> DExpr -> Walk
+walk _ (BoundVar 0) = Walk (Just 0) "0"
+walk _ (BoundVar v) = Walk Nothing (string (show v))
+walk _ (FreeVar v) = Walk Nothing (string v)
+walk p (DApp f x) =
+  let wf = walk appP f
+      wx = walk argP x
+      ch = case f of
+        BoundVar 1 -> succ <$> num wx
+        _ -> Nothing
+   in Walk ch (_paren p appP (pstr wf <> " " <> pstr wx))
+walk p (DAbs (DAbs body)) =
+  let wb = walk topP body
+   in Walk Nothing $ case num wb of
+        Just n -> string (show n)
+        Nothing -> _paren p lamP ("λ.λ." <> pstr wb)
+walk p (DAbs m) =
+  Walk Nothing (_paren p lamP ("λ." <> pstr (walk topP m)))
+
+_paren :: Int -> Int -> TextBuilder -> TextBuilder
+_paren p q t
+  | p > q = "(" <> t <> ")"
+  | otherwise = t
